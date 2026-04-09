0709cb95ff0c64db85b2f3d76bfb830910e2cc3f
Octogene <ooctogene@gmail.com>
2026-04-09 10:40:41 +0200

    chore: update lint files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,17 @@ import globals from 'globals';
 
 export default [
     {
-        files: ['content.js'],
+        files: [
+            "content/constants.js",
+            "content/utils.js",
+            "content/cache.js",
+            "content/api.js",
+            "content/resolver.js",
+            "content/badge.js",
+            "content/playlist.js",
+            "content/observers.js",
+            "content/main.js"
+        ],
         languageOptions: {
             ecmaVersion: 2022,
             sourceType: 'script', // content.js is an IIFE, not a module
